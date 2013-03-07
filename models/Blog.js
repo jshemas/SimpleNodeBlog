f4d9ad3ecc3fae35714f303e94f5371a003a0950
Josh <joshshemas@gmail.com>
2013-03-06 23:11:27 -0500

    added blog list dropdown

--- models/Blog.js
+++ models/Blog.js
@@ -34,7 +34,7 @@ module.exports = function(mongoose) {
 	// gets all blog entry(s)
 	var getBlogPost = function(callback) {
 		var query = Blog.find();
-		query.select('title body _id');
+		query.select('title body _id createdDate');
 		//execute the query at a later time
 		query.exec(function (err, blog) {
 		if (err) return handleError(err);
