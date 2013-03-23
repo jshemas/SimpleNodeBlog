278b7770cc7ecd3252062d03469da1676786cd2a
Josh <joshshemas@gmail.com>
2013-03-22 21:52:41 -0400

    clean up facebook.js

--- public/js/facebook.js
+++ public/js/facebook.js
@@ -23,43 +23,31 @@ window.fbAsyncInit = function() {
 }(document));
 
 function testAPI(blogEvent) {
-	console.log('Welcome!  Fetching your information.... ');
 	FB.api('/me', function(response) {
-		console.log("blogEvent3:",blogEvent);
-		console.log('Good to see you, ' + response.name + '.');
 		response.blogEvent = blogEvent;
 		$.ajax({
 			type: "POST",
 			url: '/fbEvent',
 			data: response,
 			success: function(data) {
-				console.log("data:",data);
+				console.log(data);
 			}
 		});
-		return response;
 	});
 }
 
 function login(blogEvent) {
-	console.log("FB, login");
 	FB.login(function(response) {
-		console.log("FB, login enter");
-		console.log("blogEvent2:",blogEvent);
 		if (response.authResponse) {
-			console.log("FB, login if");
 			// connected
-			var testapi = testAPI(blogEvent);
-			logout()
-			return testapi;
+			testAPI(blogEvent);
 		} else {
-			console.log("FB, login else");
 			// cancelled
 		}
 	});
 }
 
 function logout() {
-	console.log("FB, logout");
 	FB.logout(function(response) {
 		// user is now logged out
 	});
@@ -67,17 +55,14 @@ function logout() {
 
 function getLoginStatus(blogEvent) {
 	FB.getLoginStatus(function(response) {
-		console.log("blogEvent1:",blogEvent);
 		if (response.status === 'connected') {
 			//response.status === 'connected' will be true whenever the User viewing the page is both logged into Facebook and has already previously authorized the current app.
-			console.log("FB, if");
+			testAPI(blogEvent);
 		} else if (response.status === 'not_authorized') {
 			// response.status === 'not_authorized' is true whenever the User viewing the page is logged into Facebook, but has not yet authorized the current app. 
-			console.log("FB, else if");
 			login(blogEvent);
 		} else {
 			// The final else statement is true when the User viewing the page is not logged into Facebook, and therefore the state of their authorization of the app is unknown.
-			console.log("FB, else");
 			login(blogEvent);
 		}
 	});
